e222c3566ff68018ae9e4232492b57d8457a4771
Daniil Potemkin <daniilpot@live.com>
2025-05-14 22:34:30 +0300

    transfer

--- src/App.tsx
+++ src/App.tsx
@@ -27,24 +27,6 @@ function App() {
           <h1 className="text-4xl mb-4 font-light">Наталья и Даниил</h1>
           <p className="text-xl text-gray-200 mb-8">Дорогие гости, с радостью приглашаем вас на нашу свадьбу</p>
           <div className="flex items-center justify-center">
-            {/* <span className="relative flex size-3 mr-3">
-              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
-              <span className="relative inline-flex size-3 rounded-full bg-red-500"></span>
-            </span>
-            <a href="#transfer">
-              <p className="text-2xl text-gray-200">Информация о трансфере</p>
-            </a> */}
-
-            {/* <a href="#transfer" className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
-              <span className="relative flex size-3 mr-3">
-                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
-                <span className="relative inline-flex size-3 rounded-full bg-red-500"></span>
-              </span>
-              <span className="w-full">Информация о трансфере</span>
-              <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-              </svg>
-            </a> */}
             <span className="relative inline-flex">
               <a href="#transfer" className="inline-flex items-center rounded-md bg-white px-4 py-2 text-sm leading-6 font-semibold text-gray-200 ring-1 ring-gray-900/10 transition duration-150 ease-in-out dark:bg-white/5 dark:ring-white/20">
                 Информация о трансфере
